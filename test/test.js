607a286a26b52ed9c3b97e3e949c1ec1738f1bee
Ekpenyong <okpo.ekpenyong@gmail.com>
2019-12-12 21:55:04 +0100

    test for admin/employee sign-in

--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
-const app = require('../app')
+const app = require('../app');
+const should = require('should');
 const request = require('supertest')
+const agent = request.agent(server);
 
 chai.use(chaiHttp);
 const expect  = chai.expect;
@@ -14,25 +16,29 @@ const userCredentials = {
 
 // first check mocha
 const assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
+describe('Array', () => {
+  describe('#indexOf()', () => {
     it('should return -1 when the value is not present', function() {
       assert.equal([1,2,3].indexOf(4), -1);
     });
   });
 });
 
-// first justify that our app  exists
+// justify that our app  exists
 describe('App basics', () => {
   it('Should exists', () => {
     expect(app).to.be.a('function');
   })
 })  
 
-
-// Using request.agent() is the key
-var agent = request.agent(server);
-
-const resolvingPromise = new Promise( (resolve) => {
-  resolve('promise resolved!');
-});
+describe('Login Sessions', () => {
+  it('Authenticated Admin/Employee should sign in', (done) => {
+    agent.post('/api/auth/signin')
+    .send({ email: 'ubong.emma2@example.com', password: 'ubongemma122' })
+    .end((err, res) => {
+      expect(
+        res.status).to.equal(200);
+      done();
+    });
+  })
+})
