e211a794e07e1f6cce4a246bf10b5939ef8e117b
Ekpenyong <okpo.ekpenyong@gmail.com>
2019-12-13 22:50:46 +0100

    modify test script

--- test/test.js
+++ test/test.js
@@ -9,11 +9,12 @@ const agent = request.agent(server);
 chai.use(chaiHttp);
 const expect  = chai.expect;
 
-const userCredentials = {
-  email: 'jamesd2.dean@example.com', 
-  password: 'jamesdean2'
-}
-
+const resolvingPromise = new Promise((resolve) =>
+  resolve('promise resolved')
+);
+const rejectingPromise = new Promise((resolve, reject) =>
+  reject(new Error('promise rejected'))
+);
 // first check mocha
 const assert = require('assert');
 describe('Array', () => {
@@ -32,9 +33,17 @@ describe('App basics', () => {
 })  
 
 describe('Login Sessions', () => {
-  it('Authenticated Admin/Employee should sign in', async () => {
-  const response = await request(app).post('/api/auth/signin')
-    .send({ email: 'ubong.emma2@example.com', password: 'ubongemma122' });
-      expect(response.status).to.equal(200);
-  });
+    it('assertion success', async () => {
+      const result = await resolvingPromise;
+      expect(result).to.equal('promise resolved');
+    });
+
+    it('Authenticated Admin/Employee should sign in', async () =>{
+      const result = await chai.request(server)
+         .post('/api/auth/signin') 
+         .send({ email: 'ubong.emma2@example.com', password: 'ubongemma122' });
+         expect(result.status).to.equal(200);   
+
+    });
+
 });
